e39e446f6f8f61694ac3a1424309d9fa7ab7de96
DGhonyan <87140425+DGhonyan@users.noreply.github.com>
2023-06-28 15:50:44 +0400

    create database and it's user

--- requirements/mariadb/entrypoint.sh
+++ requirements/mariadb/entrypoint.sh
@@ -5,12 +5,15 @@ service mysql restart
 sleep 5;
 
 mysql << EOF
-ALTER USER 'root'@'localhost' IDENTIFIED BY '1';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@localhost IDENTIFIED BY '$MYSQL_PASSWORD';
+CREATE DATABASE IF NOT EXISTS '$MYSQL_DATABASE';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';
 exit
 EOF
 
-sleep 100;
+service mysql stop
 
-#service mysql stop
+sleep 5;
 
-#/usr/sbin/mysqld
+/usr/sbin/mysqld
